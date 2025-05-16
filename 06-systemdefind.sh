8239afa24de6a5498afebfd71121864193798261
vinodh241 <a.vinodh9072@gmail.com>
2025-05-16 15:44:45 +0530

    system defind varaiables

---
+++ 06-systemdefind.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#system defined varaibles
+#all system defind varaibles are uppercase 
+<<systemdefinedvaraibles
+$HISTSIZE
+
+username=$USER
+  
+systemdefinedvaraibles
+
+
+echo "systemdefined varaiables"
+
+HistorySize=$HISTSIZE
+username=$USER
+
+echo "The histsize is : "$HISTSIZE
+ehco "The user is : "$USER
+echo "The present working directory is :"$pwd
+
+
