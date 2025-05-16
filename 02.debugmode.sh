207901b0885b2988ec419c5e3aed621914a1dcfb
vinodh241 <a.vinodh9072@gmail.com>
2025-05-16 11:45:51 +0530

    shell-script-2

---
+++ 02.debugmode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Running shell script in debugmode. if you run shell script in debugmode we can easly findout the errors ( line by line we can see the out put)
+
+echo "Hello Vinodh"
+echo "Running shell-scripting on debug mode"
+set -x
+echo "Now this line running on debugmode"
+set -x
+echo "The above lines executed on debug mode"
