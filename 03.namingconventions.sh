40419e73a42a4142ebd80af37818a50b4df8ccd3
vinodh241 <a.vinodh9072@gmail.com>
2025-05-16 12:15:53 +0530

    Filenameconventions

--- 03.namingconventions.sh
+++ 03.namingconventions.sh
@@ -6,9 +6,14 @@
 #file system is case sensetive
 #some of the valid filename conventions are  below
 
+echo "some of the valid filename conventions are  below"
+
 echo "vinodh.sh"
 echo "vinod.sh"
 echo "Vinodh.sh"
 echo "Vinodh_20250516.sh"
 echo "Vinodh_19.sh"
+
+echo "Dont use special charcters"
+
 
