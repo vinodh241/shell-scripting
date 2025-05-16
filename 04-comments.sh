6861e6c127a3d49635e406f4c975801cfaeb2f7d
vinodh241 <a.vinodh9072@gmail.com>
2025-05-16 13:09:30 +0530

    comments

---
+++ 04-comments.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Shell script supporting 2 types of comments
+
+#single line comment and multiline comment
+
+ #  (#) --> single line comment indicates with yash
+ 
+ # <<vinodh 
+                # Multiline comments starts with <<anyname  and ends with same name as below
+ # vinodh 
+
+ echo "Welcome to S L C scripting"
+ echo "shell scripting supports SLC and MLC"
+ #echo "This line commented with S L C"
+ echo "we can use M L C for more lines"
+ <<vinodh
+ date
+ ls
+ cal
+ cat
+ vinodh
+ echo "comments are used above lines"
