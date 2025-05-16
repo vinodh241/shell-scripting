07431a8fb6a139ed4082c022daef847985d4a255
vinodh241 <a.vinodh9072@gmail.com>
2025-05-16 11:59:00 +0530

    Debugmode

---
+++ 02-debugmode1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Debugmode 
+
+echo "This is the frist line"
+echo "Now im running the below lines on debugmode"
+set -x
+echo "Hello welcome to the sjel-practice"
+set +x
+echo "now the above lines are running on debug mode"
+echo "check the error's on debug lines"
