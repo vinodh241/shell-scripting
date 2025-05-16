ab23319ffc236b30137b336c645a5322f1db4257
vinodh241 <a.vinodh9072@gmail.com>
2025-05-16 15:10:16 +0530

    userdefind varaiables

---
+++ 05-varaibles.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#Varaibles = 
+<<Varaiables
+
+Varaiables
+# varaibles 2 types
+# System define varaibles and user define variables
+# How to see all system define varaibles ?
+#( env or printenv )
+# pwd (lowercase) command name
+#PWD is Variable name
+#echo HISTSIZE 
+#export HISTSIZE=200 (override the default values)
+#User defind var's
+<<UV
+     a=10
+     b=20
+     c=30
+UV
+
+
+echo "User defind varaibles"
+
+a=10
+b=20
+c=30
+name=vinodh
+varaibales=userdefind varaibales
+
+echo "a varaibles value is:"$a
+echo "b varaibles value is:"$b
+echo "c varaibles value is:"$C
+echo "name varaible value is:"$name
+echo "varaibles value is"$Varaibles
+date
+echo "The above line status is:"$!
+echo "Number of varaibles is:"$#
+
+
+
+
+
+
+ 
