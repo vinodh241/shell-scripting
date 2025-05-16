cdd3335c03f9c5b47435a238f60eeeb2a2985853
vinodh241 <a.vinodh9072@gmail.com>
2025-05-16 12:14:34 +0530

    Filenameconventions

---
+++ 03.namingconventions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Naming conventions
+#file can contain 256 charcters
+#The name may contain Alphabets, digits,dot,underscores.
+#file system is case sensetive
+#some of the valid filename conventions are  below
+
+echo "vinodh.sh"
+echo "vinod.sh"
+echo "Vinodh.sh"
+echo "Vinodh_20250516.sh"
+echo "Vinodh_19.sh"
+
