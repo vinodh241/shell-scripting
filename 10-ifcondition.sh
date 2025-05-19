ac6cf89d2538b2a79350783c7efe378fa326b118
vinodh241 <a.vinodh9072@gmail.com>
2025-05-19 16:06:46 +0530

    ifcondition

--- 10-ifcondition.sh
+++ 10-ifcondition.sh
@@ -36,37 +36,34 @@ echo "ifelse condition"
 
 # fi
 
-USERID=$( id -u )
+#!/bin/bash
+
+USERID=$(id -u)
+
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
-    exit 1
-    else 
+    exit 1 #give other than 0 upto 127
+else
     echo "You are running with root access"
 fi
-    dnf list installed mysql
 
-if [ $? -ne 0 ]   
+dnf list installed mysql
 
-    echo "MYSQL is not instaled ... Going to install"
-
-    else 
-
-    echo "MySQL is already installed ... nothing to do"
-    exit 1
-    dnf install mysql -y  
-if [ $? -eq 0 ]
- then 
-    ehco "Installing MySQL is ... SUCCESS"
+# check already installed or not. if Installed $? is 0, then 
+# If not installed $? is not 0. expression is true
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed... going to install it"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installing MySQL is ... SUCCESS"
     else
-    echo "Installing MuSQL is .... FAILURE"
-    exit 1
-fi 
-
-  else
-    echo "MYSQL is already installed .. Nothing to do"
-    exit 1 
+        echo "Installing MySQL is ... FAILURE"
+        exit 1
+    fi
+else
+    echo "MySQL is already installed...Nothing to do"
 fi
-
-
 
