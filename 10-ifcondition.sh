dd9e3bf93690586fda3200f137ca073d4b9590e0
vinodh241 <a.vinodh9072@gmail.com>
2025-05-19 15:42:43 +0530

    ifcondition

--- 10-ifcondition.sh
+++ 10-ifcondition.sh
@@ -23,16 +23,40 @@ IFCONDITION
 
 echo "ifelse condition"
 
-NUMBER=$1
+# NUMBER=$1
 
-if [ $NUMBER -lt 10 ]
-then
+# if [ $NUMBER -lt 10 ]
+# then
 
-echo "given number $NUMBER is less then 10 "
+# echo "given number $NUMBER is less then 10 "
 
-else 
+# else 
 
-echo "givem number $NUMBER is not less than 10 "
+# echo "givem number $NUMBER is not less than 10 "
 
-fi
+# fi
+
+USERID=$( id -u )
+if [ $USERID -ne 0 ]
+        then
+        echo "ERROR:: Please run this script with root access"
+        exit 1
+        else 
+        echo "You are running with root access"
+        fi
+dnf lis installed mysql
+if [ $? -eq 0 ]
+        then 
+        ehco "Installing MySQL is ... SUCCESS"
+        else
+        echo "Installing MuSQL is .... FAILURE"
+
+        exit 1
+        fi 
+  else
+        echo "MYSQL is already installed .. Nothing to do"
+        exit 1 
+    fi
+
+
 
