8397bf2358ede28add7baee3b7ac9dbd6ac8a2db
vinodh241 <a.vinodh9072@gmail.com>
2025-05-19 14:54:47 +0530

    ifcondition

---
+++ 10-ifcondition.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# ifconditon
+<<IFCONDITION
+
+syntax:
+
+if (){
+then 
+
+-----------
+---------------
+
+
+else
+    
+    ------------------
+
+}
+fi
+
+IFCONDITION
+
+echo "ifelse condition"
+
+NUMBER=$1
+
+if [ NUMBER -nt 10 ]
+then
+
+echo "given number $NUMBER is less then 10 "
+
+else 
+
+echo "givem number $NUMBER is not less than 10 "
+
+fi
+
