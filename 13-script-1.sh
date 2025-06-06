08a9c12cab0d98329a8369bf8f6e4456363382aa
vinodh241 <a.vinodh9072@gmail.com>
2025-06-06 12:23:15 +0530

    shell-scripting

---
+++ 13-script-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+GREETING="Hello vinodh"
+echo "$GREETING"
+echo "PID Of Script-1 : " $$
+
+./script-2.sh
---
+++ 14-scripts-2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "PID of script-2 is : " $$
+
+echo "$GREETING"
